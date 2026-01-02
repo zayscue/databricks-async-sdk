87478d17993a2f0b9fd4a3b7a466fb688e044a0d
Zackery Ayscue <ztayscue@gmail.com>
2026-01-02 16:18:40 -0500

    qf.

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,6 @@ mod tests {
 
     #[test]
     fn test_config_builder() {
-        // This is a test
         let config = Config {
             client_id: String::from("test-client-id"),
             secret: String::from("test-secret"),
